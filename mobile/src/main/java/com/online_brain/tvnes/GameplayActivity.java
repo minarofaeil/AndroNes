ab01bc5fc629ad546f35ed4aa3bb2ee3092b3da7
Mina Rofaeil <minashokry@gmail.com>
2016-06-20 21:41:19 -0400

    Make an android project and remove non-android classes.

---
+++ mobile/src/main/java/com/online_brain/tvnes/GameplayActivity.java
@@ -0,0 +1,13 @@
+package com.online_brain.tvnes;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameplayActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gameplay);
+    }
+}
