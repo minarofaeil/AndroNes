b5dc530aabf7e5dcdd28e1b75c2c6308d7630389
Mina Rofaeil <minashokry@gmail.com>
2016-06-21 09:36:19 -0400

    Initial implementation of Android UI.

--- mobile/src/main/java/com/online_brain/tvnes/GamePlayActivity.java
+++ mobile/src/main/java/com/online_brain/tvnes/GamePlayActivity.java
@@ -3,11 +3,20 @@ package com.online_brain.tvnes;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class GameplayActivity extends Activity {
+import com.grapeshot.halfnes.NES;
+
+public class GamePlayActivity extends Activity {
+
+    private NES nes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gameplay);
+
+	    HalfNesAndroidUI halfNesAndroidUI = (HalfNesAndroidUI) findViewById(R.id.game_play_view);
+	    nes = new NES(halfNesAndroidUI);
+
+
     }
 }
